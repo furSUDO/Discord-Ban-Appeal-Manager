791a1eb39a9b55ee2fb781da4aa5b10805f070a9
fursudo <tommysmurray@gmail.com>
2021-09-21 16:22:53 +0200

    Added requirement for KICK_MEMBERS

--- commands/cases/deny.js
+++ commands/cases/deny.js
@@ -27,13 +27,13 @@ module.exports = {
                                 message.guild.members.fetch(userID, {force: true} ).then(async guildMember => {
                                     let appealServer = client.guilds.cache.get(result[0].appealServer);
                                     let parentServer = client.guilds.cache.get(result[0].parentServer);
-                                    if (!(appealServer.me.permissions.has(['MANAGE_CHANNELS','MANAGE_ROLES','MANAGE_GUILD','BAN_MEMBERS','MANAGE_MESSAGES']) || parentServer.me.permissions.has(['MANAGE_CHANNELS','MANAGE_ROLES','MANAGE_GUILD','BAN_MEMBERS','MANAGE_MESSAGES']))) {
+                                    if (!(appealServer.me.permissions.has(['MANAGE_CHANNELS','MANAGE_ROLES','MANAGE_GUILD','BAN_MEMBERS','KICK_MEMBERS','MANAGE_MESSAGES']) || parentServer.me.permissions.has(['MANAGE_CHANNELS','MANAGE_ROLES','MANAGE_GUILD','BAN_MEMBERS','MANAGE_MESSAGES']))) {
                                         message.reply({
                                             embeds: [{
                                                 color: 15088700,
                                                 title:`<:warning:847479424583729213> WARNING <:warning:847479424583729213>`,
                                                 description:`Please make sure I have all of the following permissions enabled in both ${parentServer.name}, and here.`,
-                                                image:{url:`https://cdn.discordapp.com/attachments/756644176795533334/858360385022197780/DBAM_REQUIRED_PERMISSION_SCOPE.png`}
+                                                image:{url:`https://cdn.discordapp.com/attachments/756644176795533334/866994443529224192/DBAM_REQUIRED_PERMISSION_SCOPE.png`}
                                             }]
                                         })
                                         return;
@@ -83,7 +83,62 @@ module.exports = {
                                             }).catch(e=>console.log(e)).finally(message.channel.delete().catch(e=>console.log(e)))
                                         }
                                     }).catch(e=>console.log(e));
-                                }).catch(e=>message.reply(`This is not an active case channel.`));
+                                }).catch(e=>{
+                                    message.reply(`This member has left the Appeal server, attempting to proceed with action.`)
+                                    let appealServer = client.guilds.cache.get(result[0].appealServer);
+                                    let parentServer = client.guilds.cache.get(result[0].parentServer);
+                                    if (!(appealServer.me.permissions.has(['MANAGE_CHANNELS','MANAGE_ROLES','MANAGE_GUILD','BAN_MEMBERS','KICK_MEMBERS','MANAGE_MESSAGES']) || parentServer.me.permissions.has(['MANAGE_CHANNELS','MANAGE_ROLES','MANAGE_GUILD','BAN_MEMBERS','MANAGE_MESSAGES']))) {
+                                        message.reply({
+                                            embeds: [{
+                                                color: 15088700,
+                                                title:`<:warning:847479424583729213> WARNING <:warning:847479424583729213>`,
+                                                description:`Please make sure I have all of the following permissions enabled in both ${parentServer.name}, and here.`,
+                                                image:{url:`https://cdn.discordapp.com/attachments/756644176795533334/866994443529224192/DBAM_REQUIRED_PERMISSION_SCOPE.png`}
+                                            }]
+                                        })
+                                        return;
+                                    }
+                                    message.guild.bans.create(userID).catch(e=>{})
+                                    const parentGuild = client.guilds.cache.find(guilds => guilds.id === parentServer.id);
+                                    message.channel.messages.fetch({ limit: 100 })
+                                    .then(async messages =>{
+                                        guildMember = await client.users.fetch(userID)
+                                        await fsPromises.mkdir(`./api/archives/${parentServer.id}/${guildMember.id}/`,{ recursive: true })
+                                        const writeStream = fs.createWriteStream(`./api/archives/${parentServer.id}/${guildMember.id}/${uuidv4()}.txt`);
+                                        const pathName = writeStream.path;
+                                        writeStream.write(`Ban Appeal Log for ${guildMember.username} (${guildMember.id}) - ${message.channel.createdAt}\n\n`)
+                                        messages = messages.sort()
+                                        messages.forEach(message => {
+                                            writeStream.write(`[${message.channel.name}] [${message.author.id}] ${message.author.username}#${message.author.discriminator}: ${message.content}\n`)
+                                        });
+                                        writeStream.on('error', (err) => {
+                                            console.error(`There is an error writing the file ${pathName} => ${err}`)
+                                        });
+                                        writeStream.end();
+                                        //Logging
+                                        let logChannel = message.guild.channels.cache.find(channel => channel.name === "logs")
+                                        if ((typeof logChannel === 'undefined')) {
+                                        message.reply({embeds: [{color: 15088700,title:`<:warning:847479424583729213> Failed to log event`,description:`Please create a channel with the name \`logs\` or run \`!init\` to format the server properly.\n\nThis channel will not delete automatically.`,fields:[{
+                                            name:`Conversation Archive`,
+                                            value:`https://api.dbam.dev/${pathName.substring(6)}`
+                                        }]}],}).catch(e=>console.log(e))
+                                        }else{
+                                            logChannel.send({
+                                                embeds: [{
+                                                    color: 15088700,
+                                                    title:`<:warning:847479424583729213> ${guildMember.username} (${guildMember.id}) has been permanently banned from ${parentGuild.name}`,
+                                                    fields:[{
+                                                        name:`Responsible Moderator`,
+                                                        value:`${message.author.username}`
+                                                    },{
+                                                        name:`Conversation Archive`,
+                                                        value:`https://api.dbam.dev/${pathName.substring(6)}`
+                                                    }]
+                                                }]
+                                            }).catch(e=>console.log(e)).finally(message.channel.delete().catch(e=>console.log(e)))
+                                        }
+                                    }).catch(e=>console.log(e));
+                                });
                             }else{
                                 message.reply({embeds: [{color: 15548997,title:`<:DND:851523015057080340> Please run this command in the appeal server!`,description:`Due to it's destructive nature, this command can only be run in an appeal server.`}],})
                             }
