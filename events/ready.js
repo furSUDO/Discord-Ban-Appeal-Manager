10818d1c8be1a23e4667b16d1445c36180d0d081
Whanos <github@sergal.fun>
2021-09-24 09:18:54 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(`Ready! Logged in as ${client.user.tag}.`);
 		client.user.setPresence({ activities: [{ name: `over ${Math.floor(client.guilds.cache.size/2)} servers!`, type:'WATCHING'}], status: 'dnd' });
 	},
 };
