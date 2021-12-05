e6ef89b2da286261931a8feb9cb3f8dbd98df489
furSUDO <tommysmurray@gmail.com>
2021-12-05 02:05:22 +0200

    updated events

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,15 +1,16 @@
 module.exports = {
 	name: 'guildCreate',
-	execute(server,client,con) {
-		var sql = `INSERT INTO servers (serverID) VALUES ('${server.id}')`;
-		con.query(sql, function (err) {
-			if (err){
-				if (err.code === 'ER_DUP_ENTRY'){
-					console.log("Bot joined a pre-existing server.");
+	execute(server, client, con) {
+		const sql = `INSERT INTO servers (serverID) VALUES ('${server.id}')`;
+		con.query(sql, function(err) {
+			if (err) {
+				if (err.code === 'ER_DUP_ENTRY') {
+					console.log('Bot joined a pre-existing server.');
 				}
-			}else{
-				console.log("Bot joined a new server.");
+			}
+			else {
+				console.log(`Bot joined a new server. (${server.name})`);
 			}
 		});
-    },
+	},
 };
